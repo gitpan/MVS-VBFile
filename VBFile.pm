dad73c2ade450b6f5aed38a36c8a6ed87a6a21bf
Geoffrey Rommel <grommel@cpan.org>
2000-08-18 18:49:21 +0000

    Import of GROMMEL/MVS-VBFile-0.03 from CPAN.
    gitpan-cpan-distribution: MVS-VBFile
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         GROMMEL/MVS-VBFile-0.03.tar.gz
    gitpan-cpan-author:       GROMMEL
    gitpan-cpan-maturity:     released

--- VBFile.pm
+++ VBFile.pm
@@ -8,9 +8,10 @@ require Exporter;
 
 @ISA = qw(Exporter);
 @EXPORT = qw(vbget);
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 my $blib = 0;  # Bytes left in block
+$MVS::VBFile::bdws = 0;
 
 #--- vbget gets a single record; if called in array context (the user
 #--- wants all records in a single array), it calls vbget_array.
@@ -22,8 +23,9 @@ sub vbget {
  }
 
  my ($bdw, $rdw, $reclen, $v_record, $n);
- if ($blib == 0) {
+ if ($blib == 0 && $MVS::VBFile::bdws) {
 	#--- Beginning of a block: read the Block Descriptor Word
+	#--- if we've been told to.
     $n = read($FH, $bdw, 4);
     if ($n < 4) {  # End of file
        return undef();
@@ -33,6 +35,7 @@ sub vbget {
 	#--- Now read the Record Descriptor Word
  $n = read($FH, $rdw, 4);
  if ($n < 4) {
+    return undef() if ! $MVS::VBFile::bdws;  # End of file
     Carp::carp "vbget: Unexpected end of file";
     return undef();
  }
@@ -42,7 +45,8 @@ sub vbget {
  if ($n != $reclen) {
     Carp::carp "vbget: Unexpected end of file";
  }
- $blib = $blib - ($reclen + 4);
+ $blib = $blib - ($reclen + 4)  if $MVS::VBFile::bdws;
+
  return $v_record;
 }
 
@@ -54,8 +58,9 @@ sub vbget_array {
  my @out = ();
 
  while (1) {
-    if ($blib == 0) {
+    if ($blib == 0 && $MVS::VBFile::bdws) {
 	#--- Beginning of a block: read the Block Descriptor Word
+	#--- if we've been told to.
        $n = read($FH, $bdw, 4);
        if ($n < 4) {  # End of file
           return @out;
@@ -65,6 +70,7 @@ sub vbget_array {
 	#--- Now read the Record Descriptor Word
     $n = read($FH, $rdw, 4);
     if ($n < 4) {
+       return @out if ! $MVS::VBFile::bdws;  # End of file
        Carp::carp "vbget: Unexpected end of file";
        return @out;
     }
@@ -75,7 +81,7 @@ sub vbget_array {
        Carp::carp "vbget: Unexpected end of file";
        return @out;
     }
-    $blib = $blib - ($reclen + 4);
+    $blib = $blib - ($reclen + 4)  if $MVS::VBFile::bdws;
     push @out, $v_record;
  }
 }
@@ -101,7 +107,8 @@ a mainframe MVS file in variable blocked (VB) format.  It works like
 the angle operator: when called in scalar context, it returns the next
 record; when in array context, it returns the entire file in a single
 array.  The file must be in "binary" format (no translation of bytes)
-and include the block and record descriptor words.
+and include record descriptor words.  The file may include block
+descriptor words but need not.
 
 The rationale behind this is as follows.  Most files from MVS
 systems are either fixed-length (record format FB) or variable-length
@@ -122,12 +129,33 @@ Read the file as follows:
   # and then process the array (only on small files, of course).
   close FILEHANDLE;
 
+=head1 VARIABLE
+
+The variable B<MVS::VBFile::bdws> tells the module whether the file
+to be read contains block descriptor words.  The default is 0 (false);
+set it to 1 or any other true value if the file contains BDW's.
+
 =head1 RESTRICTIONS
 
-VB is the only format supported.  In particular, this function will
-not work properly on format V (unblocked variable-length) or VBS
-(spanned).  Since VB is by far the most commonly used format, this
-should not be a major snag.
+VB (blocked) and V (unblocked) formats are supported.  This function
+will not work properly on format VBS (spanned).  Since VB is by far
+the most commonly used format, this should not be a major snag.
+
+=head1 MORE ABOUT DESCRIPTOR WORDS
+
+Record descriptor words are 4 bytes that appear at the beginning of
+each record in a VB file.  The first two bytes contain the record
+length in binary (16 bits, signed, big-endian); the last two are used
+only by spanned records and are ignored by this module.  Block
+descriptor words, likewise, are 4 bytes that appear at the beginning
+of each block, having the same format.
+
+My experience with FTP from MVS is limited, but it seems that if you
+transfer a file from an MVS host via FTP including the RDW's, the
+RDW's will be transferred but the BDW's will not.  Most applications
+do not require BDW's, but if you want them, they can be transferred
+by converting the VB file to undefined records (recfm=U) under MVS
+and then transferring the converted file.
 
 =head1 AUTHOR
 
--- test.pl
+++ test.pl
@@ -3,10 +3,7 @@
 
 ################## We start with some black magic to print on failure.
 
-# Change 1..1 below to 1..last_test_to_print .
-# (It may become useful if the test is moved to ./t subdirectory.)
-
-BEGIN { $| = 1; print "1..1\n"; }
+BEGIN { $| = 1; print "1..5\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use MVS::VBFile;
 $loaded = 1;
@@ -14,8 +11,34 @@ print "ok 1\n";
 
 ################## End of black magic.
 
-#--- Test scalar call.
-open(VB, "./mvs_vb.testfile") or die "Could not open: $!";
+#--- Test scalar call (no BDW's).
+open(VB, "./mvsvb1.txt") or die "Could not open: $!";
+while ($r = vbget(*VB)) {
+   $n++;
+   $rec1 = $r  if $n == 1;
+}
+if ($n == 3 && substr($rec1,0,4) eq "\xC2\x85\x88\x96") {
+   print "ok 2\n";
+} else {
+   print "not ok 2\n";
+}
+close VB;
+
+#--- Test array call (no BDW's).
+open(VB, "./mvsvb1.txt") or die "Could not open: $!";
+$n = 0;
+@aa = vbget(*VB);
+if (@aa == 3 && substr($aa[2],0,4) eq "\x60\x60\xC9\x40") {
+   print "ok 3\n";
+} else {
+   print "not ok 3\n";
+}
+close VB;
+
+
+#--- Test scalar call (with BDW's).
+$MVS::VBFile::bdws = 1;
+open(VB, "./mvsvb2.txt") or die "Could not open: $!";
 while ($r = vbget(*VB)) {
    $n++;
    $rec1 = $r  if $n == 1;
@@ -23,20 +46,20 @@ while ($r = vbget(*VB)) {
 }
 if ($n == 20 && substr($rec1,0,4) eq "\xC2\x93\x85\xA2"
      && substr($rec20,0,4) eq "\x40\x40\xE3\x88") {
-   print "ok 2\n";
+   print "ok 4\n";
 } else {
-   print "not ok 2\n";
+   print "not ok 4\n";
 }
 close VB;
 
-#--- Test array call.
-open(VB, "./mvs_vb.testfile") or die "Could not open: $!";
+#--- Test array call (with BDW's).
+open(VB, "./mvsvb2.txt") or die "Could not open: $!";
 $n = 0;
 @aa = vbget(*VB);
 if (@aa == 20 && substr($aa[0],0,4) eq "\xC2\x93\x85\xA2"
      && substr($aa[19],0,4) eq "\x40\x40\xE3\x88") {
-   print "ok 3\n";
+   print "ok 5\n";
 } else {
-   print "not ok 3\n";
+   print "not ok 5\n";
 }
 close VB;
